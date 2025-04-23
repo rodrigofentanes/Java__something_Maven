652217f8ab119def0aa8e0a06d0bdd08a68d0951
Rodrigo Fentanes <rodrigo.fentanes@hotmail.com>
2025-04-22 23:55:32 -0300

    Some changes to mount Package and create a executable Jar file

---
+++ archdata/notes.md
@@ -0,0 +1,5 @@
+Para rodar o pacote basta fazer:
+
+```java
+  java -jar .\mavenproject-1.0-SNAPSHOT.jar
+```
--- src/main/java/com/fentanesinc/Main.java
+++ src/main/java/com/fentanesinc/Main.java
@@ -5,23 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 import com.fentanesinc.Model.ResponseSearchMovieDTO;
+import com.fentanesinc.Services.TmdbServices;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("https://api.themoviedb.org/3/search/movie?query=matrix&include_adult=true&language=en-US"))
-        .setHeader("accept", "application/json")
-        .setHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNGViNjk5N2E4YjM3NjhhZDc5M2ZkZGNmOTYxNTE0NCIsIm5iZiI6MTYwNTEzNjMzNi4xODksInN1YiI6IjVmYWM2ZmQwZmNiOGNjMDA0MGY4NzczNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j_J0q5nvtJMlt5wB6prMvYY305nFyPjBkSIlNskp3X8")
-        .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Scanner scan = new Scanner(System.in);
+		System.out.println("Digite o nome do filme que deseja pesquisar:");
+		String name = scan.next();
+        scan.close();
         
-        System.out.println(response.body());
-
-        ResponseSearchMovieDTO responseSearchMovieTO = new ResponseSearchMovieDTO().parse(response.body());
+        ResponseSearchMovieDTO responseSearchMovieTO = TmdbServices.getInstance().serachMovieByName(name);
 
         for (ResponseSearchMovieDTO.Result result : responseSearchMovieTO.results) {
             System.out.println("#####################################");
